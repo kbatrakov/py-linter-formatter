ca23a7088061c10fe082c722d519c542591f6f65
kbatrakov <kirillbatrakov0819@gmail.com>
2025-01-15 23:47:22 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,61 +1,3 @@
-error = {
-    "code": "E501",
-    "filename": "./source_code_2.py",
-    "line_number": 18,
-    "column_number": 80,
-    "text": "line too long (99 > 79 characters)",
-    "physical_line": '    return f"I like to filter, rounding, doubling, '
-    "store and decorate numbers: {', '.join(items)}!\"",
-}
-
-
-errors = [
-    {
-        "code": "E501",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 80,
-         "text": "line too long (99 > 79 characters)",
-        "physical_line": '    return f"I like to filter, rounding, doubling, ' 
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-    {
-        "code": "W292",
-        "filename": "./source_code_2.py",
-        "line_number": 18,
-        "column_number": 100,
-        "text": "no newline at end of file",
-        "physical_line": '    return f"I like to filter, rounding, doubling, '
-        "store and decorate numbers: {', '.join(items)}!\"",
-    },
-]
-
-report_file = {
-    "./test_source_code_2.py": [],
-    "./source_code_2.py":
-        [
-            {
-                "code": "E501",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 80,
-                "text": "line too long (99 > 79 characters)",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-            {
-                "code": "W292",
-                "filename": "./source_code_2.py",
-                "line_number": 18,
-                "column_number": 100,
-                "text": "no newline at end of file",
-                "physical_line": '    return f"I like to filter, rounding, doubling, '
-                "store and decorate numbers: {', '.join(items)}!\"",
-            },
-        ]
-}
-
-
 def format_linter_error(error: dict) -> dict:
     return {"line": error["line_number"],
             "column": error["column_number"],
@@ -72,6 +14,3 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(key, value) for key, value in linter_report.items()]
-
-
-
